6d7028c24a4770bfc32c6987c03acf5f48e2de7d
MarioH1919 <jonas.hellberg13@gmail.com>
2026-01-28 19:09:48 +0100

    Hitboxes
    Hitboxen und Npc

--- Game engine/GameEngine.py
+++ Game engine/GameEngine.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
 import pygame
-import time
 
 
 
@@ -20,12 +19,23 @@ sprite = filedialog.askopenfilename(
 
 
 
-#bildrendering
+
+#npc path
+npc = "Sprites/Costumes/CHIKORITA.png"
+
+
+#location from sprite on sprite sheet
 x = 0
 y = 0
 
+#npc location auf sprite sheet
+npc_sprite_x = 0
+npc_sprite_y = 0
 b = 64 #sprite breite
 h = 64 #sprite höhe
+
+
+
 #display settings
 dx = 640
 dy = 640
@@ -33,6 +43,15 @@ dy = 640
 #spritelocation
 sprite_x = 0
 sprite_y = 0
+
+#npc location
+npc_x = 64
+npc_y = 64
+
+# hitboxes
+sprite_hitbox = pygame.Rect(sprite_x, sprite_y, 64, 64)
+npc_hitbox = pygame.Rect(npc_x, npc_y, 64, 64)
+
 
 
 speed = 1 #sprite speed
@@ -44,8 +63,10 @@ scrn = pygame.display.set_mode((dx, dy))
 
 
 
-pygame.display.set_caption('Character animation')
-img = pygame.image.load(sprite).convert_alpha()
+pygame.display.set_caption("Super-TMS-Game Engine")
+sprite = pygame.image.load(sprite).convert_alpha()
+npc = pygame.image.load(npc).convert_alpha()
+
 
 
 
@@ -57,11 +78,23 @@ while status:
     # background
     scrn.fill([0, 128, 0])
 
+    scrn.blit(npc, (npc_x, npc_y), (npc_sprite_x, npc_sprite_y, b, h))
+    scrn.blit(sprite, (sprite_x, sprite_y), (x,y,b,h))
 
+    #debug
+    pygame.draw.rect(scrn, (255, 0, 0), sprite_hitbox, 1)
+    pygame.draw.rect(scrn, (0, 0, 255), npc_hitbox, 1)
 
-    scrn.blit(img, (sprite_x, sprite_y), (x,y,b,h))
+    # hitbox update
+    sprite_hitbox.x = sprite_x
+    sprite_hitbox.y = sprite_y
 
+    npc_hitbox.x = npc_x
+    npc_hitbox.y = npc_y
 
+    # collision check
+    if sprite_hitbox.colliderect(npc_hitbox):
+        print("touched")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -86,6 +119,9 @@ while status:
             sprite_y -= speed
     pygame.display.flip()
     clock.tick(60)
+
+
+
 
 
 pygame.quit()
