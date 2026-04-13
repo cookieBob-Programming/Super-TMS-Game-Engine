0bd3bd2a243d6dd18f387714d984451091a39386
MarioH1919 <256412215+MarioH1919@users.noreply.github.com>
2026-04-13 10:27:30 +0200

    Actor edit
    Kekse

--- src/tmsge/Actor.py
+++ src/tmsge/Actor.py
@@ -1,20 +1,37 @@
-from abc import ABC, abstractmethod
-
-class Actor(ABC):
+import pygame
+class bob:
+    def bob_rede(self,kekse):
+        self.kekse = kekse
+        print("ich bin bob")
+pygame.init()
+class Actor(bob):
     '''
         Ein Actor verändert das Aussehen des Spiels = aktives Geschehen
     '''
     
-    @abstractmethod
+
     def tick(self):
         '''
             Etwas passiert
         '''
         pass
-    
-    @abstractmethod
-    def draw(self):
+
+    def init(self,actor_x,actor_y,x,y,b,h,sprite):
+        self.actor_x = actor_x
+        self.actor_y = actor_y
+        self.x = x
+        self.y = y
+        self.b = b
+        self.h = h
+        self.sprite = sprite#optional fals actor ein sprite hat
+
+
+    def draw(self,sprite_x,sprite_y,x,y,b,h,sprite):
+
+
         '''
             Gibt ein pygame-Surface dieses Actors zurück
         '''
-        pass
+        pygame.scrn.blit(sprite, (sprite_x, sprite_y), (x, y, b, h))
+
+
